db45f9bb174d0198c88f2d6e7ca6659d2858ad6d
e.alzoubi <e.alzoubi@classera.com>
2025-01-04 18:38:20 +0300

    some coe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboadItemComponent } from "./dashboard/dashboad-item/dashboad-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboadItemComponent],
 })
 export class AppComponent {
 
