85544f685438d91652b5ef337ac1e3d745accf84
e.alzoubi <e.alzoubi@classera.com>
2025-01-04 14:53:07 +0300

    first commit

---
+++ src/app/heade/heade.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heade',
+  standalone: true,
+  imports: [],
+  templateUrl: './heade.component.html',
+  styleUrl: '/src/styles.css'
+})
+export class HeadeComponent {
+
+}
